8247fc60839a9863700d37913ba1e14f0eb5f397
Nicolò Arena <nicoloarena46@gmail.com>
2024-04-05 21:05:27 +0200

    initial setup

---
+++ src/main/java/dev/nicoloarena/cinemille/CineMilleApplication.java
@@ -0,0 +1,13 @@
+package dev.nicoloarena.cinemille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/nicoloarena/cinemille/CineMilleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nicoloarena.cinemille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
