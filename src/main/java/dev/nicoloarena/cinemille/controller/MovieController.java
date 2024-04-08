69a719d0c79c9145ce3cb80da00212e63eea2ee3
Nicolò Arena <nicoloarena46@gmail.com>
2024-04-08 23:22:58 +0200

    add @CrossOrigin to MovieController

--- src/main/java/dev/nicoloarena/cinemille/controller/MovieController.java
+++ src/main/java/dev/nicoloarena/cinemille/controller/MovieController.java
@@ -3,6 +3,7 @@ package dev.nicoloarena.cinemille.controller;
 import dev.nicoloarena.cinemille.dto.MovieDTO;
 import dev.nicoloarena.cinemille.service.MovieService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
+@CrossOrigin(origins = "*") // todo add cors configuration
 public class MovieController {
 
     private final MovieService movieService;
